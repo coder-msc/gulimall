8710510772b93e771783041984092c3c56f7622e
scma <1771947201@qq.com>
2021-09-22 23:46:02 +0800

    引入alibaba,完成服务的注册（nacos）

---
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 骑蜗牛的羊
+ * @date 2021/9/22 23:38
+ * <p>启动类</p>
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallCouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class);
+    }
+}
