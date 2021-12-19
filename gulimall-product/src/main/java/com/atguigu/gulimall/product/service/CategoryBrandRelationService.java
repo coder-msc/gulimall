9d26ac07a4e833415150e26de6ed7a60f028282e
scma <59968873@qq.com>
2021-12-19 23:23:20 +0800

    关联分类，分页插件配置

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/CategoryBrandRelationService.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/CategoryBrandRelationService.java
@@ -1,9 +1,11 @@
 package com.atguigu.gulimall.product.service;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.gulimall.product.entity.CategoryBrandRelationEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +18,23 @@ import java.util.Map;
 public interface CategoryBrandRelationService extends IService<CategoryBrandRelationEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+    /**
+     * @Author 骑蜗牛的羊
+     * @Description:获取某个分类下所有品牌信息
+     * @Date 2021/12/19 23:17
+     * @param catId
+     * @return void
+     */
+    List<BrandEntity> getBrandsByCatId(Long catId);
+
+
+    /**
+     * @Author 骑蜗牛的羊
+     * @Description:根据获取品牌id 、三级分类id查询对应的名字保存到数据库
+     * @Date 2021/12/19 23:18
+     * @param categoryBrandRelation
+     * @return void
+     */
+    void saveDetail(CategoryBrandRelationEntity categoryBrandRelation);
 }
 
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/BrandVo.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author 骑蜗牛的羊
+ * @date 2021/12/19 23:14
+ * <p>商品</p>
+ */
+@Data
+public class BrandVo {
+    private Long brandId;
+
+    private String brandName;
+}
